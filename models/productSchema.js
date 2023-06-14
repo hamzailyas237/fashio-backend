9147c71c7a3a429af02e86d1f1065d4c835fc4da
hamzailyas237 <hamzailyas237@gmail.com>
2023-06-14 15:11:53 +0500

    added update product api and configure code formatter

--- models/productSchema.js
+++ models/productSchema.js
@@ -1,35 +1,39 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const productSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    // required: true
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  stock: [
+    {
+      size: String,
+      quantity: Number,
     },
-    description: {
-        type: String,
-        // required: true
+  ],
+  images: [
+    {
+      original: String,
+      thumbnail: String,
     },
-    category: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    stock: [{
-        size: String,
-        Quantity: Number,
-    }],
-    images: [{
-        orignal: String,
-        thumbnail: String,
-    }],
-    created_at: {
-        type: Date,
-        default: Date.now,
-    },
+  ],
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
 })
 
-const productModel = mongoose.model('product', productSchema)
+const productModel = mongoose.model("product", productSchema)
 module.exports = productModel
--- routers/router.js
+++ routers/router.js
@@ -1,20 +1,22 @@
 const {
-    addProduct,
-    deleteProduct,
-    getProductsByCategory,
-    getAllProducts,
-    getProductById
-} = require('../controllers/productController')
-const { addOrder, getAllOrders } = require('../controllers/orderController')
+  addProduct,
+  deleteProduct,
+  getProductsByCategory,
+  getAllProducts,
+  getProductById,
+  updateProductById,
+} = require("../controllers/productController")
+const { addOrder, getAllOrders } = require("../controllers/orderController")
 
-const router = require('express').Router()  
+const router = require("express").Router()
 
-router.post('/product', addProduct)
-router.delete('/product/:id', deleteProduct)
-router.get('/products/:category', getProductsByCategory)
-router.get('/product/:id', getProductById)
-router.get('/products', getAllProducts)
-router.post('/order', addOrder)
-router.get('/orders', getAllOrders)
+router.post("/product", addProduct)
+router.delete("/product/:id", deleteProduct)
+router.get("/products/:category", getProductsByCategory)
+router.get("/product/:id", getProductById)
+router.put("/product/:id", updateProductById)
+router.get("/products", getAllProducts)
+router.post("/order", addOrder)
+router.get("/orders", getAllOrders)
 
 module.exports = router
